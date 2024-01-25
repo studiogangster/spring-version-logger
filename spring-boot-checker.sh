d8ce56ce407b9c6a05421c1eb64adaedb65b3dc4
Prakhar Agnihotri <rounak316@gmail.com>
2024-01-25 17:28:50 +0530

    Update spring-boot-checker.sh

--- spring-boot-checker.sh
+++ spring-boot-checker.sh
@@ -5,18 +5,16 @@ search_directory="/"
 
 # Find all JAR and WAR files
 find "$search_directory" -type f \( -name "*.jar" -o -name "*.war" \) | while read -r file; do
-    echo "Checking $file for Spring Boot..."
-
     if [[ "$file" == *.jar ]]; then
         # For JAR files, check the manifest
         if unzip -p "$file" META-INF/MANIFEST.MF | grep -q 'Spring-Boot-Version'; then
-            spring_boot_version=$(unzip -p "$file" META-INF/MANIFEST.MF | grep 'Spring-Boot-Version' | cut -d':' -f2 | tr -d ' ')
+            spring_boot_version=$(unzip -p "$file" META-INF/MANIFEST.MF | awk -F': ' '/Spring-Boot-Version/ {print $2}' | tr -d '[:space:]')
             echo "$file (Version: $spring_boot_version)" >> results.txt
         fi
     elif [[ "$file" == *.war ]]; then
         # For WAR files, check the MANIFEST.MF in WEB-INF
         if unzip -p "$file" WEB-INF/MANIFEST.MF | grep -q 'Spring-Boot-Version'; then
-            spring_boot_version=$(unzip -p "$file" WEB-INF/MANIFEST.MF | grep 'Spring-Boot-Version' | cut -d':' -f2 | tr -d ' ')
+            spring_boot_version=$(unzip -p "$file" WEB-INF/MANIFEST.MF | awk -F': ' '/Spring-Boot-Version/ {print $2}' | tr -d '[:space:]')
             echo "$file (Version: $spring_boot_version)" >> results.txt
         fi
     fi
